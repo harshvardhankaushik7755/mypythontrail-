2f443a622cd5d5a5e06b4446ba43e31db3aaa386
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2023-12-25 16:27:00 +0530

    created a fully fledged calculator

--- Project1/arthematicoperationpractice.py
+++ Project1/arthematicoperationpractice.py
@@ -25,35 +25,42 @@ def cube(a):
 if __name__ == "__main__":
     l = None
     while(l!="exit"):
+        print("If your operation requires a single parametre press 'O', if multi parametre operation press 'M'")
         l = input()
-        print("")
-        if (l == "add" or l == "sub" or l=="mul" or l=="div"):
-            print("enter first number")
-            a = input()
-            print("enter second no.(note: if you have taken square or cube operation print the same no. you putted on the first one)")
-            b = input()    
-            a = int(a)
-            b = int(b)
-            if l=="add":
-                add(a, b)
-            if l=="sub":
-                sub(a, b)
-            if l=="mul":
-                mul(a, b)
-            if l == "div":
-                div(a, b)
+        if l == "M":
+            print("Choose any one operation from :'add', 'sub', 'mul', 'div'")
+            l = input()
+            if(l == "add" or l == "sub" or l=="mul" or l=="div"):
                 
-        elif l=="square" or l=="cube":
-            print("enter the number")
-            a = input()
-            a = int(a)
-            if l=="square":
-                square(a)
-            if l=="cube":
-                cube(a)
+                print("Enter first no:")
+                a = input()
+                print("Enter second no:")
+                b = input()    
+                a = int(a)
+                b = int(b)
+                if l=="add":
+                    add(a, b)
+                if l=="sub":
+                    sub(a, b)
+                if l=="mul":
+                    mul(a, b)
+                if l == "div":
+                    div(a, b)
+        elif l == "O":
+            print("Chose operations in cube or square")
+            l = input()    
+            if l=="square" or l=="cube":
+                print("enter the number")
+                a = input()
+                a = int(a)
+                if l=="square":
+                    square(a)
+                if l=="cube":
+                    cube(a)
                 
         elif l!="exit":
             print("whatever you have written is not a correct operation")
+            
         else:
             print("THANK YOU FOR USING THIS CALCULATOR")
             break
