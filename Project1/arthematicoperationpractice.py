8304d6a4401977e12094df8090e22e589011954c
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2023-12-26 18:04:57 +0530

    created a calculatar with power percentage and more operations

--- Project1/arthematicoperationpractice.py
+++ Project1/arthematicoperationpractice.py
@@ -1,7 +1,9 @@
+import math 
+
 def add(a, b):
     sum = a+b
     print("addition = ", sum)
-    
+        
 def sub(a, b):
     sub = a-b
     print("difference = ", sub)
@@ -22,22 +24,44 @@ def cube(a):
     cube = a*a*a
     print("cube: ", cube)
     
+def sqrt(a):
+    square_root = math.ceil(a ** (1/2))
+    print("Square root : ", square_root)
+    
+def cube_root(a):
+    cuberoot = math.ceil(a ** (1/3))
+    print("Cube root: ", cuberoot)
+    
+def pow(a, b):
+    power = math.pow(a, b)
+    print("power: ", power)
+    
+def percentage(a, b):
+    percent = a/b*100
+    print("percentage: ", percent, "%")
+    
+def root(a, b):
+    root = math.pow(a, 1/b)
+    print("root: ", root) 
+    
 if __name__ == "__main__":
     l = None
     while(l!="exit"):
         print("If your operation requires a single parametre press 'O', if multi parametre operation press 'M'")
         l = input()
         if l == "M":
-            print("Choose any one operation from :'add', 'sub', 'mul', 'div'")
+            print("Choose any one operation from :'add', 'sub', 'mul', 'div', 'percentage', 'power', 'root'")
             l = input()
-            if(l == "add" or l == "sub" or l=="mul" or l=="div"):
+            if(l == "add" or l == "sub" or l=="mul" or l=="div" or l=="percentage" or l=="power" or l == "root"):
                 
-                print("Enter first no:")
+                print("Enter first no:, if percent take part of the whole no.")
                 a = input()
-                print("Enter second no:")
+                print("Enter second no:, if percent whole no.")
                 b = input()    
                 a = int(a)
+                a = float(a)
                 b = int(b)
+                b = float(b)
                 if l=="add":
                     add(a, b)
                 if l=="sub":
@@ -46,10 +70,17 @@ if __name__ == "__main__":
                     mul(a, b)
                 if l == "div":
                     div(a, b)
+                if l=="percentage":
+                    percentage(a, b)
+                if l=="power":
+                    pow(a, b)
+                if l == "root":
+                    root(a, b)
+                    
         elif l == "O":
-            print("Chose operations in cube or square")
+            print("Chose operations in cube, square, cube_root, sqrt and power")
             l = input()    
-            if l=="square" or l=="cube":
+            if l=="square" or l=="cube" or l== "sqrt" or l =="cube_root" or l=="power":
                 print("enter the number")
                 a = input()
                 a = int(a)
@@ -57,6 +88,11 @@ if __name__ == "__main__":
                     square(a)
                 if l=="cube":
                     cube(a)
+                if l=="sqrt":
+                    sqrt(a)
+                if l=="cube_root":
+                    cube_root(a)
+                    
                 
         elif l!="exit":
             print("whatever you have written is not a correct operation")
