c1451a9e5cdeca436d279d36dec074a0b3cfd06c
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2024-01-05 21:27:14 +0530

    finally created two patterns

--- Project1/leftangletriangle.py
+++ Project1/leftangletriangle.py
@@ -1,5 +1,10 @@
-x = int(input("no. of rows"))
-for i in range(x):
-    for j in range(i, ""):
-        print("*", end= "*")
+rows = int(input("Enter the number of rows: "))
+
+for i in range(rows):
+    for j in range(rows-i-1):
+        print(" ", end="")
+
+    for j in range(i+1):
+        print("*", end="")
+
     print()
