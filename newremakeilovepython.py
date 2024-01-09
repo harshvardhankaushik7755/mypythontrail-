2bc98029d6d4ae0058bac3610900df52dc5a13c4
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2024-01-09 12:42:56 +0530

    my name animation

---
+++ newremakeilovepython.py
@@ -0,0 +1,13 @@
+
+TEXT = "HARSH VARDHAN KAUSHIK"
+
+for i in range(30):
+    print(TEXT)
+    
+    
+for k in range(10):
+    for i in range(6):
+        print(" " * i, TEXT)
+    for i in range(6):
+        print(" "*(5-i), TEXT)
+        
