1011d95eb8d3c4c2a6fbd0b1f9430a68a988218a
harshvardhankaushik7755 <harshvardhankaushik74917@gmail.com>
2024-01-12 18:28:58 +0530

    made some changes

--- hollowtriangle.py
+++ hollowtriangle.py
@@ -1,10 +1,22 @@
-R = int(input("enter the no. of rows: "))
+# R = int(input("enter the no. of rows: "))
 
-for row in range(1,R+1):
-    for col in range(1,2*R):
+# for row in range(1,R+1):
+#     for col in range(1,2*R):
 
-        if row == R or row+col == R+1 or col-row == R-1:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
+#         if row == row or row+col == R+1 or col-row == R-1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+def hollow_triangle(rows, cols):
+  """Prints a hollow triangle with rows and cols as variables."""
+  for i in range(1, rows + 1):
+    for j in range(1, cols + 1):
+      if i == 1 or i == rows or j == 1 or j == cols or i + j == rows + 1 or j - i == rows - 1:
+        print('*', end='')
+      else:
+        print(' ', end='')
     print()
+
+
+hollow_triangle(5, 5)
--- shapesgiver.py
+++ shapesgiver.py
@@ -1,10 +1,10 @@
 
 
-def square(rows):
+def square(rows, col):
     print("Solid square: ")
     for i in range(1, rows): 
           
-        for j in range(1, rows + 1): 
+        for j in range(1, col + 1): 
             print("*", end = " ") 
   
         print() 
@@ -21,9 +21,9 @@ def rect(l, b):
         print()
         
         
-def triangle(rows):
+def triangle(rows, col):
     for i in range(rows):
-            for j in range(rows-i-1):
+            for j in range(col-i-1):
                 print("", end=" ")
                 
             for j in range(i + 1):
@@ -31,13 +31,12 @@ def triangle(rows):
                 
             print()
         
-triangle(6)
 
-def hollowsquare(num):
+def hollowsquare(row, col):
     print("Hollow square: ")
-    for row in range(1, num+1):
-        for col in range(1,num+1):
-            if row == 1 or row==num or col==num or col == 1:
+    for i in range(1, row+1):
+        for j in range(1,col+1):
+            if row == 1 or row==i or col==j or col == 1:
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
@@ -55,15 +54,6 @@ def hollowrectangle(l ,b):
                 
         print()
         
-def hollowtriangle(num):
-    for i in range(1, num+1):
-        for j in range(1, num*2):
-            if i == num or i+j == num+1 or j-i == num-1:
-                print("*", end=" ")
-            else:
-                print(" ", end=" ")
-                
-        print() 
         
         
 if __name__ == "__main__":
@@ -72,19 +62,19 @@ if __name__ == "__main__":
         print("If your shape is hollow then write H, if solid write S.")
         userinput = input()
         if userinput == "H":
-            print("if your operation requires 1 parametre press O if two press M")
-            userinput = input()
-            if userinput == "M" or "O":
-                print("choose the shape 'hollow triangle' or 'hollow square'")
+                print("choose the shape 'hollow triangle' or 'hollow square' or 'hollow rectangle'")
                 userinput = input()
-                if userinput == "hollow triangle" or userinput == "hollow square":
+                if userinput == "hollow triangle" or userinput == "hollow square" or userinput == "hollow rectangle":
                     print("enter the no. of rows")
                     userinputasnum = input()
                     userinputasnum = int(userinputasnum)
-                    if userinput == "hollow triangle":
-                        hollowtriangle(userinputasnum)
-                    elif userinput == "hollow square":
-                        hollowsquare(userinputasnum)
+                    print("Enter the no. of cols: ")
+                    userinputasnum2 = input()
+                    userinputasnum2 = int(userinputasnum2)
+                    if userinput == "hollow square":
+                        hollowsquare(userinputasnum, userinputasnum2)
+                    elif userinput == "hollow rectangle":
+                        hollowrectangle(userinputasnum, userinputasnum2)
                         
                 print("choose the shapes in hollow rectangle")
                 userinput = input()
@@ -99,31 +89,23 @@ if __name__ == "__main__":
                         hollowrectangle(userinputasnum1, userinputasnum2)
                         
         if userinput == "S":
-            print("if your operation requires 1 parametre press O if two press M")
             userinput = input()
-            if userinput == "O" or "M":
-                print("choose the shape 'solid triangle' or 'solid square'")
-                userinput = input()
-                if userinput == "solid triangle" or userinput == "solid square":
-                    print("enter the no. of rows")
-                    userinputasnum = input()
-                    userinputasnum = int(userinputasnum)
-                    if userinput == "solid triangle":
-                        triangle(userinputasnum)
-                    elif userinput == "solid square":
-                        square(userinputasnum)
+            print("choose the shape 'solid triangle' or 'solid square' or 'solid rectangle'")
+            userinput = input()
+            if userinput == "solid triangle" or userinput == "solid square" or userinput == "solid rectangle":
+                print("enter the no. of rows")
+                userinputasnum1 = input()
+                userinputasnum1 = int(userinputasnum1)
+                print("enter the no. of cols")
+                userinputasnum2 = input()
+                userinputasnum2 = int(userinputasnum2)
+                if userinput == "solid triangle":
+                    triangle(userinputasnum1, userinputasnum2)
+                elif userinput == "solid square":
+                    square(userinputasnum1, userinputasnum2)
+                elif userinput == "solid rectangle":
+                    rect(userinputasnum1, userinputasnum2)
 
-                print("choose the shapes in solid rectangle")
-                userinput = input()
-                if userinput == "solid rectangle":
-                    print("Enter the length")
-                    userinputasnum1 = input()
-                    print("Enter the breadth")
-                    userinputasnum2 = input()
-                    userinputasnum1 = int(userinputasnum1)
-                    userinputasnum2 = int(userinputasnum2)
-                    if userinput == "solid rectangle":
-                        rect(userinputasnum1, userinputasnum2)
                         
             
         elif userinput!="exit":
